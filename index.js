1cbad001a321f43fd2b8d726835f893e20761283
Alex Valencia <jorgealexandervalencia@gmail.com>
2024-04-09 11:02:49 -0500

    New file last_post_date.json to store the date of the latest article

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const fs = require('fs').promises;
+const fs = require('fs');
 const Parser = require('rss-parser');
 const parser = new Parser();
 
@@ -7,17 +7,25 @@ const ICON_SIZE = '24px';
 const LATEST_ARTICLE_PLACEHOLDER = '%{{latest-article}}%';
 
 (async () => {
-   const markdownTemplate = await fs.readFile('./README.md.tpl', { encoding: 'utf-8' });  
+   const markdownTemplate = await fs.promises.readFile('./README.md.tpl', { encoding: 'utf-8' });  
    const {items} = await parser.parseURL('https://jorgealexandervalencia.hashnode.dev/rss.xml');
    const [{title, link}] = items;
    const latestArticleMarkdown = `[${title}](${link})`;
 
    // Primero, reemplaza el marcador de posición del último artículo
-   let newMarkDowm = markdownTemplate.replace(LATEST_ARTICLE_PLACEHOLDER, latestArticleMarkdown);
+   let newMarkdown = markdownTemplate.replace(LATEST_ARTICLE_PLACEHOLDER, latestArticleMarkdown);
 
    // Luego, reemplaza el marcador de posición del tamaño de los iconos en el resultado anterior
-   newMarkDowm = newMarkDowm.replaceAll(ICONS_SIZE_PLACEHOLDER, ICON_SIZE);
+   newMarkdown = newMarkdown.replaceAll(ICONS_SIZE_PLACEHOLDER, ICON_SIZE);
 
-   console.log(newMarkDowm);
-   await fs.writeFile('./README.md', newMarkDowm);
+   console.log(newMarkdown);
+   await fs.promises.writeFile('./README.md', newMarkdown);
+
+   // Guardar la fecha del último artículo en un archivo
+   const lastPostDate = new Date().toISOString();
+   fs.writeFileSync('last_post_date.json', JSON.stringify({ date: lastPostDate }));
+
+   module.exports = {
+     lastPostDate
+   };
 })();
