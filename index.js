bfb1e13c8651786cc184916e15f39a63f0366cc7
Alex Valencia <jorgealexandervalencia@gmail.com>
2024-04-09 15:45:45 -0500

    Actualizar lógica para mostrar el último artículo

--- index.js
+++ index.js
@@ -9,23 +9,17 @@ const LATEST_ARTICLE_PLACEHOLDER = '%{{latest-article}}%';
 (async () => {
    const markdownTemplate = await fs.promises.readFile('./README.md.tpl', { encoding: 'utf-8' });  
    const {items} = await parser.parseURL('https://jorgealexandervalencia.hashnode.dev/rss.xml');
-   const [{title, link}] = items;
-   const latestArticleMarkdown = `[${title}](${link})`;
+   let latestArticle;
 
-   // Primero, reemplaza el marcador de posición del último artículo
-   let newMarkdown = markdownTemplate.replace(LATEST_ARTICLE_PLACEHOLDER, latestArticleMarkdown);
+   if(items.length > 8) {
+     latestArticle = `[${items[0].title}](${items[0].link})`; 
+   } else {
+     latestArticle = `[${items[7].title}](${items[7].link})`;
+   }
 
-   // Luego, reemplaza el marcador de posición del tamaño de los iconos en el resultado anterior
+   let newMarkdown = markdownTemplate.replace(LATEST_ARTICLE_PLACEHOLDER, latestArticle);
    newMarkdown = newMarkdown.replaceAll(ICONS_SIZE_PLACEHOLDER, ICON_SIZE);
 
    console.log(newMarkdown);
    await fs.promises.writeFile('./README.md', newMarkdown);
-
-   // Guardar la fecha del último artículo en un archivo
-   const lastPostDate = new Date().toISOString();
-   fs.writeFileSync('last_post_date.json', JSON.stringify({ date: lastPostDate }));
-
-   module.exports = {
-     lastPostDate
-   };
 })();
